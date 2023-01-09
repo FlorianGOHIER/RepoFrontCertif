b83a53dd2dd008a549f773742703713586e1241a
JeanPhilippeFerraz <jean.philippe.ferraz@hotmail.fr>
2023-01-09 12:08:55 +0100

    architecture initialisation

---
+++ easychat/src/app/icons/components/icon-day/icon-day.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-day',
+  templateUrl: './icon-day.component.html',
+  styleUrls: ['./icon-day.component.scss']
+})
+export class IconDayComponent {
+
+}
